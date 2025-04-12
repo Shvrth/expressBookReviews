f72e98520cbdcee5a4e05f5c0b57f54dedfa9815
Shvrth <raishivarth@gmail.com>
2025-04-12 01:49:10 -0400

    save

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,55 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.setHeader('Content-Type', 'application/json');
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    
+    if (book) {
+      res.setHeader('Content-Type', 'application/json');
+      return res.status(200).send(JSON.stringify(book, null, 4));
+    } 
+    return res.status(404).json({ 
+      message: `Book with ISBN ${isbn} not found` 
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorName = decodeURIComponent(req.params.author);
+  const matchingBooks = Object.values(books).filter(book => 
+    book.author.toLowerCase() === authorName.toLowerCase()
+  );
+
+  if (matchingBooks.length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  }
+  return res.status(404).json({
+    message: `No books found by author '${authorName}'`
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const Title = decodeURIComponent(req.params.title);
+    const matchingBooks = Object.values(books).filter(book => 
+      book.title.toLowerCase() === Title.toLowerCase()
+    );
+  
+    if (matchingBooks.length > 0) {
+      res.setHeader('Content-Type', 'application/json');
+      return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+    }
+    return res.status(404).json({
+      message: `No books found by author '${authorName}'`
+    });
 });
 
 //  Get book review
